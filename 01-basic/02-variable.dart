5ea6a53bf23c23afd148d80e7462fa6858a369a5
AriseMarcher <2543061999@qq.com>
2022-09-22 07:51:24 +0800

    dart相关

---
+++ 01-basic/02-variable.dart
@@ -0,0 +1,28 @@
+void main() {
+  // 声明变量
+  var age = 18;
+
+  print(age);
+
+  String name = 'xiaoming';
+
+  print(name);
+
+  dynamic myname = 20; // 动态的变量
+  print(myname);
+
+  var defaultName;
+
+  print(defaultName);
+
+  var Name = '123';
+  print(Name);
+
+  // 常量
+  const n1 = 1;
+  // n1 = 6; // 常量一旦声明 不能再改动
+  print(n1);
+
+  final n2 = 2;
+  print(n2);
+}
